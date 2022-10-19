ed169ee79482f2c57fd89bea8a6dd37a73f98606
Deepak9852 <deepakraj214125@gmail.com>
2022-10-19 18:24:33 +0530

    Dark mode commit

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ function loadCoupon(){
 function closeCoupon(){
     document.getElementById('coupon').style.display = 'None';
 }
+// var mode = document.getElementById("mode");
+// mode.onclick = function(){
+//   mode.body.classList.toggle("dark-theme");
+// }
+function myFunction(){
+    var element = document.body;
+    element.classList.toggle("dark-theme")
+}
